b21747f2b7044cc0f8616dea67a1188c8a95b0cb
MuzaffaR <abdurahmonovmuzaffar56@gmail.com>
2022-11-20 19:29:06 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// $('.hero__slider-list').slick();
+$('.hero__slider-list').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
+
+
+
+
+let itemBtn = document.querySelectorAll(".header__menu-link");
+
+itemBtn.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.classList.contains("active")) {
+      item.classList.remove("active");
+    } else {
+      item.classList.add("active");
+    }
+  });
+});
+
