b41276e4c8dd65fa3ca756c50a495a390fe20f14
MuzaffaR <abdurahmonovmuzaffar56@gmail.com>
2022-11-21 22:57:07 +0500

    cardlar yaratildi

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 // $('.hero__slider-list').slick();
-$('.hero__slider-list').slick({
+$(".hero__slider-list").slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
 });
 
-
-
-
+$(".film-list").slick({
+  slidesToShow: 6,
+  slidesToScroll: 2,
+  autoplay: false,
+  autoplaySpeed: 1500,
+});
 
 let itemBtn = document.querySelectorAll(".header__menu-link");
 
@@ -21,4 +24,3 @@ itemBtn.forEach((item) => {
     }
   });
 });
-
